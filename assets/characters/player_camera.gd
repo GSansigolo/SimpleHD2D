fca2273f859009c8b6a52b3e3d2bb907a8579372
GSansigolo <gabrielsansigolo@gmail.com>
2023-11-19 13:27:33 -0300

    Add simple hd-2d

---
+++ assets/characters/player_camera.gd
@@ -0,0 +1,53 @@
+extends SpringArm3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	handle_camera()
+	handle_zoom()
+
+func handle_camera():
+	if Input.is_action_just_pressed("rotate_left"):
+		# self.rotate_y(self.rotation.y + PI/2)
+		var tween = create_tween()
+		tween.set_trans(Tween.TRANS_SINE)
+		tween.set_ease(Tween.EASE_OUT)
+		tween.tween_property(self,"rotation:y", self.rotation.y + PI/2, 0.5)
+	elif Input.is_action_just_pressed("rotate_right"):
+		# self.rotate_y(self.rotation.y - PI/2)
+		var tween = create_tween()
+		tween.set_trans(Tween.TRANS_SINE)
+		tween.set_ease(Tween.EASE_OUT)
+		tween.tween_property(self,"rotation:y", self.rotation.y - PI/2, 0.5)
+	# reset rotation
+	elif Input.is_action_pressed("rotate_right") and Input.is_action_pressed("rotate_left"):
+		print("reset rotation")
+		var tween = create_tween()
+		tween.set_trans(Tween.TRANS_SINE)
+		tween.set_ease(Tween.EASE_OUT)
+		tween.tween_property(self,"rotation:y", PI, 0.5)
+
+func handle_zoom():
+	if Input.is_action_just_pressed("zoom_in"):
+		
+		if spring_length >0:
+			var tween = create_tween()
+			tween.set_parallel(true)
+			tween.set_trans(Tween.TRANS_SINE)
+			tween.set_ease(Tween.EASE_OUT)
+			var newlength = spring_length - 1
+			tween.tween_property(self,"spring_length", newlength, 0.5)
+			if newlength <= 0:
+				tween.tween_property(self,"position", Vector3(0,0.2,0), 0.5)
+			print("len: ", newlength)
+
+	elif Input.is_action_just_pressed("zoom_out"):
+		if spring_length <4:
+			var tween = create_tween()
+			tween.set_parallel(true)
+			tween.set_trans(Tween.TRANS_SINE)
+			tween.set_ease(Tween.EASE_OUT)
+			var newlength = spring_length + 1
+			tween.tween_property(self,"spring_length", newlength, 0.5)
+			print("len: ", newlength)
+			if newlength > 0:
+				tween.tween_property(self,"position", Vector3(0,0,0), 0.5)
---
+++ assets/characters/player_move.gd
@@ -0,0 +1,60 @@
+extends CharacterBody3D
+
+@export var animation_frame = 0
+var facing = 0
+const FRAMES = 3
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = ($SpringArm3D.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	update_facing(input_dir)
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func update_facing(direction):
+	# Start and Stop Animation based on input
+	if direction != Vector2(0,0):
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.stop()
+		animation_frame = 0
+	
+	# Set Facing and Horizontal Flip depending on input direction
+	if direction.x == -1:
+		$Sprite3D.flip_h = false
+		facing = 2
+		print("move left")
+	elif direction.x == 1:
+		$Sprite3D.flip_h = true
+		facing = 2
+		print("move right")
+	elif direction.y == 1:
+		facing = 0
+		print("move_down")
+	elif direction.y == -1:
+		facing = 1
+		print("move up")
+	
+	# Update the frame dynamically for animation
+	$Sprite3D.frame = animation_frame + (facing * FRAMES)
