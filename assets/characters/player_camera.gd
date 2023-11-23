9d71017a8a3baf3c533fa9893e26b86473db1a28
GSansigolo <gabrielsansigolo@gmail.com>
2023-11-23 11:29:29 -0300

    fix camera

--- assets/characters/player_camera.gd
+++ assets/characters/player_camera.gd
@@ -20,7 +20,6 @@ func handle_camera():
 		tween.tween_property(self,"rotation:y", self.rotation.y - PI/2, 0.5)
 	# reset rotation
 	elif Input.is_action_pressed("rotate_right") and Input.is_action_pressed("rotate_left"):
-		print("reset rotation")
 		var tween = create_tween()
 		tween.set_trans(Tween.TRANS_SINE)
 		tween.set_ease(Tween.EASE_OUT)
@@ -29,25 +28,23 @@ func handle_camera():
 func handle_zoom():
 	if Input.is_action_just_pressed("zoom_in"):
 		
-		if spring_length >0:
+		if int(spring_length)>0:
 			var tween = create_tween()
 			tween.set_parallel(true)
 			tween.set_trans(Tween.TRANS_SINE)
 			tween.set_ease(Tween.EASE_OUT)
-			var newlength = spring_length - 1
+			var newlength = int(spring_length) - 1
 			tween.tween_property(self,"spring_length", newlength, 0.5)
-			if newlength <= 0:
+			if int(newlength) <= 0:
 				tween.tween_property(self,"position", Vector3(0,0.2,0), 0.5)
-			print("len: ", newlength)
 
 	elif Input.is_action_just_pressed("zoom_out"):
-		if spring_length <4:
+		if int(spring_length)<6:
 			var tween = create_tween()
 			tween.set_parallel(true)
 			tween.set_trans(Tween.TRANS_SINE)
 			tween.set_ease(Tween.EASE_OUT)
-			var newlength = spring_length + 1
+			var newlength = int(spring_length) + 1
 			tween.tween_property(self,"spring_length", newlength, 0.5)
-			print("len: ", newlength)
-			if newlength > 0:
+			if int(newlength) > 0:
 				tween.tween_property(self,"position", Vector3(0,0,0), 0.5)
